d0051e05369afa5df2b022095db8ca16e809291c
Brett Hall <bretthall@fastmail.fm>
2021-12-06 19:55:47 -0800

    Did some clean-up

--- AkkaExample/Storage.fs
+++ AkkaExample/Storage.fs
@@ -24,6 +24,7 @@ type StorageMsg =
 let startStorageActor parent =
     
     Spawn.spawn parent "storage" (props <| fun (ctx: Actor<StorageMsg>) ->
+        
         let rec handleMsgs values msg =
             match msg with
             | Query query ->
@@ -39,9 +40,11 @@ let startStorageActor parent =
                     key = update.key
                     value = Some update.value
                 }
-                become (handleMsgs (Map.add update.key update.value values))    
+                let newValues = Map.add update.key update.value values
+                become (handleMsgs newValues)    
             | Stop ->
                 logDebug ctx "Got stop message"
                 stop ()
+                
         become (handleMsgs Map.empty)
     )
